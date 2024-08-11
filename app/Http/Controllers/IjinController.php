d233b1c3bc80eb6e6a5007dcda32f6af38ab41e1
Ahda Firly Barori <51354643+adhfry@users.noreply.github.com>
2024-08-11 16:44:46 +0700

    fix part 2

--- app/Http/Controllers/IjinController.php
+++ app/Http/Controllers/IjinController.php
@@ -8,93 +8,96 @@ use Illuminate\Http\Request;
 
 class IjinController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
-    {
-        $user = User::find($request->user_id);
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(Request $request)
+  {
+    $user = User::find($request->user_id);
+    // izin debug halaman /guru/izin meski data created (201)
+    // dd($user); tidak muncul karena ptk_id nilainya null
+    // jadi harus memiliki ptk_id
 
-        $ijins = ijin::with('user', 'kelas')->where('ptk_id', $user->ptk_id)->whereMonth('sampai', date('m'))->get();
+    $ijins = ijin::with('user', 'kelas')->where('ptk_id', $user->ptk_id)->whereMonth('sampai', date('m'))->get();
 
-        return $ijins;
-    }
+    return $ijins;
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(ijin $ijin)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(ijin $ijin)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ijin $ijin)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(ijin $ijin)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, ijin $ijin)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, ijin $ijin)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        $ijin = ijin::find($id)->delete();
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy($id)
+  {
+    $ijin = ijin::find($id)->delete();
 
-        return response()->json([], 200);
-    }
+    return response()->json([], 200);
+  }
 
-    public function simI(Request $request)
-    {
+  public function simI(Request $request)
+  {
 
-        // return $request->form['dari'];
-        $validate = $this->validate($request, [
-            "kelas_id" => "required",
-            "mapel_id" => "required",
-            "dari" => "required",
-            "sampai" => "required",
-            "ket" => "required",
-            "desk" => "required",
-        ]);
+    // return $request->form['dari'];
+    $validate = $this->validate($request, [
+      "kelas_id" => "required",
+      "mapel_id" => "required",
+      "dari" => "required",
+      "sampai" => "required",
+      "ket" => "required",
+      "desk" => "required",
+    ]);
 
-        // return $validate;
+    // return $validate;
 
-        $sim = ijin::create([
-            "ptk_id" => '41417c32-37b3-434e-a839-87433d318589',
-            "kelas_id" => $request->kelas_id,
-            "mapel_id" => $request->mapel_id,
-            "dari" => $request->dari,
-            "sampai" => $request->sampai,
-            "ket" => $request->ket,
-            "desk" => $request->desk,
-        ]);
+    $sim = ijin::create([
+      "ptk_id" => '41417c32-37b3-434e-a839-87433d318589',
+      "kelas_id" => $request->kelas_id,
+      "mapel_id" => $request->mapel_id,
+      "dari" => $request->dari,
+      "sampai" => $request->sampai,
+      "ket" => $request->ket,
+      "desk" => $request->desk,
+    ]);
 
-        return $sim;
-    }
+    return $sim;
+  }
 }
--- resources/js/views/guru/ijin/index.vue
+++ resources/js/views/guru/ijin/index.vue
@@ -1,240 +1,242 @@
 <!-- @format -->
 
 <template>
-	<div>
-		<Layout>
-			<BRow>
-				<BCol lg="6">
-					<BCard no-body>
-						<BCardHeader>
-							<BCardTitle>
-								<H5>Apps Perijinan</H5>
-							</BCardTitle>
-						</BCardHeader>
-						<BCardBody>
-							<div v-if="ptk">
-								<button
-									class="btn btn-md btn-outline-primary me-2"
-									type="button">
-									{{ ptk.nama.toUpperCase() }}
-								</button>
-							</div>
-							<div class="row">
-								<!-- <form action=""> -->
-								<div class="form-group mt-5 col-6">
-									<label>Pilih mapel</label>
-									<select
-										v-model="mapel_id"
-										class="form-control"
-										@change="getkelas">
-										<option value="">---</option>
-										<option
-											v-for="mapel in mapels"
-											:key="mapel.mata_pelajaran_id"
-											:value="mapel.mata_pelajaran_id">
-											{{ mapel.mata_pelajaran_id_str }}
-										</option>
-									</select>
-								</div>
-								<div class="form-group mt-5 col-6">
-									<label>Pilih Kelas</label>
-									<select
-										v-model="kelas_id"
-										class="form-control">
-										<option value="">---</option>
-										<option
-											v-for="kls in kelas"
-											:key="kls.rb_id"
-											:value="kls.rb_id">
-											{{ kls.rombels.nama }}
-										</option>
-									</select>
-								</div>
-								<div class="form-group mt-3 col-6">
-									<label for="">Dari</label>
-									<flat-pickr
-										v-model="dari"
-										:first-day-of-week="1"
-										lang="en"
-										confirm
-										class="form-control">
-									</flat-pickr>
-								</div>
-								<div class="form-group mt-3 col-6">
-									<label for="">Sampai</label>
-									<flat-pickr
-										v-model="sampai"
-										:first-day-of-week="1"
-										lang="en"
-										confirm
-										class="form-control">
-									</flat-pickr>
-								</div>
-								<div class="form-group mt-3">
-									<label>Pilih Keterangan</label>
-									<select
-										v-model="ket"
-										class="form-control">
-										<option value="ijin">Ijin</option>
-										<option value="sakit">Sakit</option>
-										<option value="dl">Dinas Luar</option>
-									</select>
-								</div>
-								<div class="form-group mt-3">
-									<label>Deskripsi Tugas</label>
-									<textarea
-										v-model="desk"
-										type="text"
-										class="form-control" />
-								</div>
-							</div>
-							<button
-								class="btn btn-primary mt-3"
-								@click="simpan">
-								simpan
-							</button>
-						</BCardBody>
-					</BCard>
-				</BCol>
-				<bCol lg="6">
-					<BCard np-body>
-						<BCardBody>
-							<div class="table-responsive">
-								<table class="table table-hover">
-									<thead>
-										<th>No</th>
-										<th>Nama</th>
-										<th>Ket</th>
-										<th>Kelas</th>
-										<th>Dari</th>
-										<th>Sampai</th>
-										<th>Desk</th>
-										<th>#</th>
-									</thead>
-									<tbody>
-										<tr
-											v-for="(repot, index) in reports"
-											:key="repot.id">
-											<td>{{ index + 1 }}</td>
-											<td>
-												<BBadge variant="primary">{{ repot.user.name }}</BBadge>
-											</td>
-											<td>
-												<BBadge variant="success">{{ repot.ket.toUpperCase() }}</BBadge>
-											</td>
-											<td style="width: 10%">{{ repot.kelas.nama }}</td>
-											<td>{{ repot.dari }}</td>
-											<td>{{ repot.sampai }}</td>
-											<td>{{ repot.desk }}</td>
-											<td>
-												<button
-													class="btn btn-sm btn-danger"
-													@click="hapus(repot.id)">
-													<span class="fa fa-trash"></span>
-												</button>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</BCardBody>
-					</BCard>
-				</bCol>
-			</BRow>
-		</Layout>
-	</div>
+  <div>
+    <Layout>
+      <BRow>
+        <BCol lg="6">
+          <BCard no-body>
+            <BCardHeader>
+              <BCardTitle>
+                <H5>Apps Perijinan</H5>
+              </BCardTitle>
+            </BCardHeader>
+            <BCardBody>
+              <div v-if="ptk">
+                <button
+                  class="btn btn-md btn-outline-primary me-2"
+                  type="button"
+                >
+                  {{ ptk.nama.toUpperCase() }}
+                </button>
+              </div>
+              <div class="row">
+                <!-- <form action=""> -->
+                <div class="form-group mt-5 col-6">
+                  <label>Pilih mapel</label>
+                  <select
+                    v-model="mapel_id"
+                    class="form-control"
+                    @change="getkelas"
+                  >
+                    <option value="">---</option>
+                    <option
+                      v-for="mapel in mapels"
+                      :key="mapel.mata_pelajaran_id"
+                      :value="mapel.mata_pelajaran_id"
+                    >
+                      {{ mapel.mata_pelajaran_id_str }}
+                    </option>
+                  </select>
+                </div>
+                <div class="form-group mt-5 col-6">
+                  <label>Pilih Kelas</label>
+                  <select v-model="kelas_id" class="form-control">
+                    <option value="">---</option>
+                    <option
+                      v-for="kls in kelas"
+                      :key="kls.rb_id"
+                      :value="kls.rb_id"
+                    >
+                      {{ kls.rombels.nama }}
+                    </option>
+                  </select>
+                </div>
+                <div class="form-group mt-3 col-6">
+                  <label for="">Dari</label>
+                  <flat-pickr
+                    v-model="dari"
+                    :first-day-of-week="1"
+                    lang="en"
+                    confirm
+                    class="form-control"
+                  >
+                  </flat-pickr>
+                </div>
+                <div class="form-group mt-3 col-6">
+                  <label for="">Sampai</label>
+                  <flat-pickr
+                    v-model="sampai"
+                    :first-day-of-week="1"
+                    lang="en"
+                    confirm
+                    class="form-control"
+                  >
+                  </flat-pickr>
+                </div>
+                <div class="form-group mt-3">
+                  <label>Pilih Keterangan</label>
+                  <select v-model="ket" class="form-control">
+                    <option value="ijin">Ijin</option>
+                    <option value="sakit">Sakit</option>
+                    <option value="dl">Dinas Luar</option>
+                  </select>
+                </div>
+                <div class="form-group mt-3">
+                  <label>Deskripsi Tugas</label>
+                  <textarea v-model="desk" type="text" class="form-control" />
+                </div>
+              </div>
+              <button class="btn btn-primary mt-3" @click="simpan">
+                simpan
+              </button>
+            </BCardBody>
+          </BCard>
+        </BCol>
+        <bCol lg="6">
+          <BCard np-body>
+            <BCardBody>
+              <div class="table-responsive">
+                <table class="table table-hover">
+                  <thead>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Ket</th>
+                    <th>Kelas</th>
+                    <th>Dari</th>
+                    <th>Sampai</th>
+                    <th>Desk</th>
+                    <th>#</th>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(repot, index) in reports" :key="repot.id">
+                      <td>{{ index + 1 }}</td>
+                      <td>
+                        <BBadge variant="primary">{{ repot.user.name }}</BBadge>
+                      </td>
+                      <td>
+                        <BBadge variant="success">{{
+                          repot.ket.toUpperCase()
+                        }}</BBadge>
+                      </td>
+                      <td style="width: 10%">{{ repot.kelas.nama }}</td>
+                      <td>{{ repot.dari }}</td>
+                      <td>{{ repot.sampai }}</td>
+                      <td>{{ repot.desk }}</td>
+                      <td>
+                        <button
+                          class="btn btn-sm btn-danger"
+                          @click="hapus(repot.id)"
+                        >
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </BCardBody>
+          </BCard>
+        </bCol>
+      </BRow>
+    </Layout>
+  </div>
 </template>
 
 <script>
-	import Layout from '@/layouts/main.vue';
-	import PageHeader from '@/components/page-header.vue';
-	import axios from 'axios';
-	import flatPickr from 'vue-flatpickr-component';
-	import 'flatpickr/dist/flatpickr.css';
-	export default {
-		data() {
-			return {
-				user: JSON.parse(localStorage.getItem('user')),
-				ptk: null,
-				mapels: [],
-				kelas: [],
-				reports: [],
-				mapel_id: '',
-				kelas_id: null,
-				mapel_id: '',
-				kelas_id: '',
-				dari: '',
-				sampai: '',
-				ket: '',
-				desk: '',
-			};
-		},
-		methods: {
-			async getPtk() {
-				await axios.get('/api/guru/ijin').then((res) => {
-					this.ptk = res.data;
-				});
-			},
-			async getMapels() {
-				await axios.get('/api/guru/getMapels').then((res) => {
-					this.mapels = res.data;
-				});
-			},
-			async getkelas() {
-				await axios.get('/api/guru/getGuruKelas').then((res) => {
-					this.kelas = res.data;
-					// ini di sorting dulu
-					this.kelas.sort((a, b) => a.rombels.nama.localeCompare(b.rombels.nama));
-				});
-			},
-			async simpan() {
-				await axios
-					.post('/api/guru/simI', {
-						mapel_id: this.mapel_id,
-						kelas_id: this.kelas_id,
-						dari: this.dari,
-						sampai: this.sampai,
-						ket: this.ket,
-						desk: this.desk,
-					})
-					.then((res) => {
-						console.log(res);
-					})
-					.catch((e) => {
-						console.log(e, 'error');
-					});
-			},
-			async getMe() {
-				await axios
-					.post('/api/guru/me', {
-						user_id: this.user.user_id,
-					})
-					.then((res) => {
-						this.reports = res.data;
-					})
-					.catch((e) => {
-						console.log(e);
-					});
-			},
-			async hapus(id) {
-				if (confirm('yakin menghapus ?')) {
-					await axios.delete('/api/guru/del/' + id).then((res) => {
-						this.getMe();
-					});
-				}
-			},
-		},
-		components: {
-			Layout,
-			PageHeader,
-			flatPickr,
-		},
-		created() {
-			this.getPtk();
-			this.getMapels();
-			this.getMe();
-		},
-	};
+import Layout from "@/layouts/main.vue";
+import PageHeader from "@/components/page-header.vue";
+import axios from "axios";
+import flatPickr from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+export default {
+  data() {
+    return {
+      user: JSON.parse(localStorage.getItem("user")),
+      ptk: null,
+      mapels: [],
+      kelas: [],
+      reports: [],
+      mapel_id: "",
+      kelas_id: null,
+      mapel_id: "",
+      kelas_id: "",
+      dari: "",
+      sampai: "",
+      ket: "",
+      desk: "",
+    };
+  },
+  methods: {
+    async getPtk() {
+      await axios.get("/api/guru/ijin").then((res) => {
+        this.ptk = res.data;
+        console.log(this.ptk);
+      });
+    },
+    async getMapels() {
+      await axios.get("/api/guru/getMapels").then((res) => {
+        this.mapels = res.data;
+      });
+    },
+    async getkelas() {
+      await axios.get("/api/guru/getGuruKelas").then((res) => {
+        this.kelas = res.data;
+        // ini di sorting dulu
+        this.kelas.sort((a, b) => a.rombels.nama.localeCompare(b.rombels.nama));
+      });
+    },
+    async simpan() {
+      await axios
+        .post("/api/guru/simI", {
+          mapel_id: this.mapel_id,
+          kelas_id: this.kelas_id,
+          dari: this.dari,
+          sampai: this.sampai,
+          ket: this.ket,
+          desk: this.desk,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e, "error");
+        });
+    },
+    async getMe() {
+      console.log({ user_id: this.user.user_id });
+
+      await axios
+        .post("/api/guru/me", {
+          user_id: this.user.user_id,
+        })
+        .then((res) => {
+          this.reports = res.data;
+          console.log({ "ini reports": this.reports });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    async hapus(id) {
+      if (confirm("yakin menghapus ?")) {
+        await axios.delete("/api/guru/del/" + id).then((res) => {
+          this.getMe();
+        });
+      }
+    },
+  },
+  components: {
+    Layout,
+    PageHeader,
+    flatPickr,
+  },
+  created() {
+    this.getPtk();
+    this.getMapels();
+    this.getMe();
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
